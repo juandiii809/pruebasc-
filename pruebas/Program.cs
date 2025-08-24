3c9f04371ab3e053774aa3b1d973d98780ad990a
juan <juanosorio1124226@correo.itm.edu.co>
2025-08-24 16:55:42 -0500

    proy

---
+++ pruebas/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Text.Json.Serialization;
+
+Console.WriteLine("Hello, World!");
+
+Automoviles num = new Automoviles() {num_llantas = 4, placa = "dtebjf"};
+Automoviles newnum = new Motos() { num_llantas = 2, placa = "hdhdd", nombre = "albert" };
+Console.WriteLine(newnum.num_llantas);
+
+public class Automoviles
+{
+    public int num_llantas;
+    public string? placa;
+    public List<int> motos = new List<int>();
+}
+
+public class Motos : Automoviles
+{
+    public string? nombre;
+}
